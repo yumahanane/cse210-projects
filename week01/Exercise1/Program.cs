970f0a284f2171f028c9a31cac08c4e38bf23e95
yumahanane <yuma.hanane1@gmail.com>
2025-07-01 16:00:40 +0200

    Exercise 1 solved

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // Display the user's name
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
