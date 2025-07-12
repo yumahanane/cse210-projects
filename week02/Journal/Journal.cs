0704491a422c0f171f98c297ce11e4be39042ee3
yumahanane <yuma.hanane1@gmail.com>
2025-07-13 00:18:59 +0200

    code added

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.IO.Enumeration;
+using Microsoft.VisualBasic;
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
@@ -26,17 +27,16 @@ public class Journal
     {
         using (StreamWriter outputFile = new StreamWriter(file))
         {
-            foreach (Entry entry in _entries)
+            foreach (Entry newEntry in _entries)
             {
-                outputFile.WriteLine(entry);
+                outputFile.WriteLine($"{newEntry._date} - Prompt: {newEntry._promptText}");
+                outputFile.WriteLine($"{newEntry._entryText}");
             }
         }
-
-
     }
 
     public void LoadFromFile(string file)
     {
-
+        string[] lines = System.IO.File.ReadAllLines(file);
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -7,36 +7,88 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
 
+        // Create an instance of Journal
         Journal theJournal = new Journal();
 
-        Entry anEntry = new Entry();
+        int choice = 0;
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
 
-        PromptGenerator promptList = new PromptGenerator();
-        promptList._prompts.Add("Who was the most interesting person I interacted with today?");
-        promptList._prompts.Add("What was the best part of my day?");
-        promptList._prompts.Add("How did I see the hand of the Lord in my life today?");
-        promptList._prompts.Add("What was the strongest emotion I felt today?");
-        promptList._prompts.Add("If I had one thing I could do over today, what would it be?");
-        promptList._prompts.Add("How did you show compassion to others? How can you extend that same compassion to yourself?");
+            // Create the menu
+            List<string> menu = new List<string>();
+            menu.Add("1. Write");
+            menu.Add("2. Display");
+            menu.Add("3. Load");
+            menu.Add("4. Save");
+            menu.Add("5. Quit");
 
-        string chosenPrompt = promptList.GetRandomPrompt();
+            // Display the menu
+            menu.ForEach(Console.WriteLine);
 
-        anEntry._promptText = chosenPrompt;
+            // Get response from user
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(chosenPrompt);
-        anEntry._entryText = Console.ReadLine();
+            if (choice == 1)
+            {
+                // Create an instance of Entry and populate it
+                Entry anEntry = new Entry();
+                string dateText = "";
+                string thePrompt = "";
+                string response = "";
 
-        DateTime theCurrentTime = DateTime.Now;
-        string dateText = theCurrentTime.ToShortDateString();
-        anEntry._date = dateText;
+                // Generate the date
+                DateTime theCurrentTime = DateTime.Now;
+                dateText = theCurrentTime.ToShortDateString();
 
-        theJournal.AddEntry(anEntry);
+                anEntry._date = dateText;
 
-        theJournal.DisplayAll();
+                // Create an instance of PromptGenerator and populate it 
+                PromptGenerator promptsList = new PromptGenerator();
+                promptsList._prompts.Add("Who was the most interesting person I interacted with today?");
+                promptsList._prompts.Add("What was the best part of my day?");
+                promptsList._prompts.Add("How did I see the hand of the Lord in my life today?");
+                promptsList._prompts.Add("What was the strongest emotion I felt today?");
+                promptsList._prompts.Add("If I had one thing I could do over today, what would it be?");
 
+                // Generate a random prompt
+                // This assigns a value to the variable. That value is generated randomly
+                // using the GetRandomPrompt method. Then, it prints out in the console 
+                // that value, that is the chosen string.
+                thePrompt = promptsList.GetRandomPrompt();
+                anEntry._promptText = thePrompt;
+                Console.WriteLine(thePrompt);
 
-        Console.WriteLine("What is the filename?");
-        string filename = Console.ReadLine();
-        theJournal.SaveToFile(filename);
+                // Get answer from user
+                response = Console.ReadLine();
+                anEntry._entryText = response;
+
+                // Append the entry to the journal
+                theJournal.AddEntry(anEntry);         
+            }
+
+            else if (choice == 2)
+            {
+                theJournal.DisplayAll();
+            }
+
+            else if (choice == 4) //save
+            {
+                Console.WriteLine("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                theJournal.SaveToFile(filename);
+            }
+
+            else if (choice == 3) //load
+            {
+                Console.WriteLine("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                theJournal.LoadFromFile(filename);
+            }
+        }
+
     }
 }
