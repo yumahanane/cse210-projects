744142f0f0c99cacd433342ec678484aa9b687e6
yumahanane <yuma.hanane1@gmail.com>
2025-07-17 23:30:50 +0200

    steps 1- 4 done

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom; ;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_top}/{_bottom}");
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        fraction1.Display();
+
+        Fraction fraction2 = new Fraction(6);
+        fraction2.Display();
+
+        Fraction fraction3 = new Fraction(6, 7);
+        fraction3.Display();
+
     }
 }
