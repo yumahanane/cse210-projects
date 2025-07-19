7fe417a1859f5750497f6ba8c129699a816e2d37
yumahanane <yuma.hanane1@gmail.com>
2025-07-20 00:29:21 +0200

    scripture memorizer completed

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,44 +9,86 @@ class Program
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
         Console.WriteLine();
 
+        // Showing Creativity and Exceeding Requirements:
+        // The program asks the user if they want a single verse scripture ou multiple verse scripture
+        Console.Write("Would you like a 'single' or 'multiple verses' scripture? ");
+        string choice = Console.ReadLine();
 
-        Reference reference = new Reference("James", 1, 5, 6);
-
-        string scriptureText = "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him. But let him ask in faith, nothing wavering. For he that wavereth is like a wave of the sea driven with the wind and tossed.";
-        //string scriptureText = "If any of you lack wisdom, let him ask of God.";
-
-
-        Scripture aScripture = new Scripture(reference, scriptureText);
-
-
-        string response = "";
-
-        while (response != "quit")
+        if (choice == "single")
         {
+            Reference reference = new Reference("James", 1, 5);
 
-            bool programRunning = aScripture.IsCompletelyHidden();
+            string scriptureText = "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him.";
+            Scripture aScripture = new Scripture(reference, scriptureText);
 
-            if (programRunning != true)
+            string response = "";
+
+            while (response != "quit")
             {
 
-                Console.WriteLine(aScripture.GetDisplayText());
-                Console.WriteLine();
+                bool programRunning = aScripture.IsCompletelyHidden();
 
-                Console.Write("Please enter to continue or type 'quit' to finish: ");
-                response = Console.ReadLine();
+                if (programRunning != true)
+                {
 
-                aScripture.HideRandomWords(4);
+                    Console.WriteLine(aScripture.GetDisplayText());
+                    Console.WriteLine();
+
+                    Console.Write("Please enter to continue or type 'quit' to finish: ");
+                    response = Console.ReadLine();
+
+                    aScripture.HideRandomWords(4);
 
 
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine(aScripture.GetDisplayText());
-                response = "quit";
+                    Console.Clear();
+                }
+                else
+                {
+                    response = "quit";
+                }
+
             }
 
         }
+        else
+        {
+            Reference reference = new Reference("James", 1, 5, 6);
+
+            string scriptureText = "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him. But let him ask in faith, nothing wavering. For he that wavereth is like a wave of the sea driven with the wind and tossed.";
+            Scripture aScripture = new Scripture(reference, scriptureText);
+            
+            string response = "";
+
+            while (response != "quit")
+            {
+
+                bool programRunning = aScripture.IsCompletelyHidden();
+
+                if (programRunning != true)
+                {
+
+                    Console.WriteLine(aScripture.GetDisplayText());
+                    Console.WriteLine();
+
+                    Console.Write("Please enter to continue or type 'quit' to finish: ");
+                    response = Console.ReadLine();
+
+                    aScripture.HideRandomWords(4);
+
+
+                    Console.Clear();
+                }
+                else
+                {
+                    response = "quit";
+                }
+            }
+        
+        }
+
+
+
+        
 
 
     }
