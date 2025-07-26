cf0907b4ddbf4b155856e11674a577cde249d0dd
yumahanane <yuma.hanane1@gmail.com>
2025-07-26 22:24:52 +0200

    comments added

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -7,9 +7,9 @@ public class Order
     {
 
     }
-    public void SetCustomer(Customer Customer)
+    public void SetCustomer(Customer customer)
     {
-        _customer = Customer;
+        _customer = customer;
     }
 
     public void AddProduct(Product product)
@@ -20,13 +20,19 @@ public class Order
 
     public void DisplayAll()
     {
+        //Create a list that will hold the cost of each product
         List<double> costList = new List<double>();
 
+        //Iterate through the list of products 
+        //Call a Production method to get the product cost
+        //Add each one to the list created above
         foreach (Product product in _products)
         {
             costList.Add(product.TotalProductCost());
         }
 
+        //Call a Customer method to check if the customer lives in the USA
+        //to set the shipping cost
         int shippingCost = 0;
 
         if (_customer.LiveInUSA() == true)
@@ -38,10 +44,14 @@ public class Order
             shippingCost = 35;
         }
 
+        //Calculate the total price of the order by adding the cost of each product;
+        // then adding to it the shipping cost
         double totalOrderCost = costList.Sum() + shippingCost;
         Console.WriteLine($"Total Price: {totalOrderCost}");
         Console.WriteLine();
 
+        //Iterate through the product list
+        //Display the name and id of each product
         Console.WriteLine($"Packing Label: ");
         foreach (Product product in _products)
         {
@@ -49,6 +59,7 @@ public class Order
         }
         Console.WriteLine();
 
+        //Display the name and address of the customer
         Console.WriteLine($"Shipping Label: ");
         _customer.Display();
 
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -9,33 +9,32 @@ class Program
 
         Console.WriteLine("Order 1");
 
+        // Create instances of Product class
         Product product1 = new Product("Banana", "1289M", 12.80, 5);
         Product product2 = new Product("Tomato", "1290P", 10.50, 10);
         Product product3 = new Product("Cucumber", "1291K", 7.20, 6);
 
-        //Address address1 = new Address("Broadway Steet", "New York City", "New York", "USA");
-
+        // Create instance of Customer class
         Customer customer1 = new Customer("Mary Hernandez", "Broadway Street, New York City, New York, USA");
 
+        //Create instance of Order and add the Product instances to it
         Order order1 = new Order();
         order1.AddProduct(product1);
         order1.AddProduct(product2);
         order1.AddProduct(product3);
 
+        //Link the Customer instance to the Order instance
         order1.SetCustomer(customer1);
 
+        //Display the total price, packing label, and shipping label
         order1.DisplayAll();
         
         // -----------------------------------------------
         Console.WriteLine();
         Console.WriteLine("Order 2");
 
-
         Product product4 = new Product("Beef", "PE10061", 49.90, 2);
         Product product5 = new Product("Prawn", "W2104", 40, 30);
-
-
-        //Address address2 = new Address("Fernando Pedro", "Chimoio", "Mania", "Mozambique");
 
         Customer customer2 = new Customer("Sheila Pedro", "Fernando Noronha Street, Chimoio, Manica, Mozambique");
 
@@ -52,13 +51,9 @@ class Program
         Console.WriteLine();
         Console.WriteLine("Order 3");
 
-
         Product product6 = new Product("Pan", "56M01", 11.90, 1);
         Product product7 = new Product("Knife", "8YN90", 5.50, 3);
         
-
-        //Address address3 = new Address("1040 Printech Ave", "Honeydew", "Johannesburg", "South Africa");
-
         Customer customer3 = new Customer("Alvaro Khumalo", "1040 Printech Ave, Honeydew, Johannesburg, South Africa");
 
         Order order3 = new Order();
